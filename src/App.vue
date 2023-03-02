21adc1564a19d5c9ab797cafa1642681b3be39cc
CheonOkHui <cjsdhrgml000@gmail.com>
2023-03-02 12:52:43 +0900

    오류 해결

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,7 @@
   
   <div class="wrapper" id="App">
     <Header/>
-    <div class="container">
       <router-view></router-view>
-    </div>
-
-    <div class="visual-bg">
-      <div class="container hero">
-          <div class="row">
-              <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
-                  <h1>Online lecture HomePage</h1>
-                  <p>Acorn 2조</p><button
-                      class="btn btn-light btn-lg action-button" type="button">Learn More</button>
-              </div>
-              <div class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none phone-holder">
-                  <div class="iphone-mockup"><img src="" class="device">
-                      <div class="screen"></div>
-                  </div>
-              </div>
-          </div>
-        </div>
-      </div>
     </div>
   <Footer/>
   
