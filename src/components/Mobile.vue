123547b3e5a923c2c3d5312de2138c580b6e5a09
Seo dong min <sdm067@naver.com>
2023-03-03 11:59:26 +0900

    lecture 상세화

---
+++ src/components/Mobile.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+	  <div v-for="front in list.body.data" :key="front">  {{ front.list}}</div>
+	  <div>{{list.body}}</div>
+	  <div>{{list.body.totalPage}}</div>
+	  <div>{{list.body.currentPage}}</div>
+	  <div>{{list.body.limit}}</div>
+	</div>
+  </template>
+  <script>
+  import axios from 'axios';
+  export default {
+	  name: 'Front',
+	  data(){
+		  return{
+			  list: []
+		  }
+	  },
+	  created() {
+	  var vm = this;
+	  axios.get('http://localhost:9000/project/api/lecture/lectureList?largeCategory=mobile&smallCategory=all')
+		.then(function(response) {
+		  console.log(response);
+		  vm.list = response.data;
+		})
+		.catch(function(error) {
+		  console.log(error);
+		})
+	},
+  }
+  </script>
+  <style>
+  </style>
