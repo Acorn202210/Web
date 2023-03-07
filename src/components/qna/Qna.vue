fe9c06e1efb39f4bd7c0651abf1de69f7f7da0f6
syCHOI <sychoi.me@gmail.com>
2023-03-07 15:53:48 +0900

    1:1문의 목록 css적용

--- src/components/qna/Qna.vue
+++ src/components/qna/Qna.vue
@@ -1,28 +1,38 @@
 <template>
-  <div>
-    <div>
-      <button type="button">등록</button>
+
+  <div class="sub-tit-box">
+    <div class="container">
+      <h3 class="sub-tit">1:1문의</h3>
     </div>
+  </div>
+    
+  <div class="container">
     <div>
-      <table>
+      <button class="btn btn-sm me-2 mb-3 button"  type="button">등록</button>
+    </div>         
+    <div class="table-responsive table-top">
+      <table class="table table-hover align-middle">
         <thead>
-          <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>등록일</th>
-            <th>답변여부</th>
+          <tr style="text-align: center" on-click="">
+            <th width="15%">번호</th>
+            <th width="auto">제목</th>
+            <th width="15%">작성자</th>
+            <th width="25%">등록일</th>
+            <th width="15%">답변여부</th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for="item in list.body.data" :key="item.boardQuestionNum">
+        <tbody class="table-group-divider">
+          <tr style="text-align: center;" v-for="item in list.body.data" :key="item.boardQuestionNum">
             <td>{{item.boardQuestionNum}}</td>
             <td>
               <a href="/qna/detail/${item.boardQuestionNum}">{{item.title}}</a>
             </td>
             <td>{{item.boardQuestionWriter}}</td>
             <td>{{item.userRegdate}}</td>
-            <td>{{item.answeredYn}}</td>
+            <td>
+              <span style="color:green;" v-if="item.answeredYn == 'Y'">답변완료</span>
+              <span style="color:orangered;" v-else>답변대기</span>              
+            </td>
           </tr>
         </tbody>
 
