2fb5f2e284555eb3bc996b1f048cdf56c4b46050
seo0601 <116258575+seo0601@users.noreply.github.com>
2023-03-17 17:19:27 +0900

    lectureList css 수정

--- src/components/lecture/lectureList.vue
+++ src/components/lecture/lectureList.vue
@@ -1,51 +1,63 @@
 <template>
-  <div class="container" style="position: relative; left: 100px;">
-    <div style="position: relative;">
-      <lectureSide style="position: fixed; left: 65px;" />
+  <div class="wrapper">
+    <div class="sub-tit-box">
+      <div class="container">
+        <h3 class="sub-tit" v-if="smallCategory !== 'all'">{{ smallCategory }}</h3>
+        <h3 class="sub-tit" v-if="smallCategory === 'all'">{{ largeCategory }}</h3>
+      </div>
     </div>
-    <p v-if="smallCategory !== 'all'">{{ smallCategory }}</p>
-    <p v-if="smallCategory === 'all'">{{ largeCategory }}</p>
-    <div>
-      <a href="/lectureUploadForm">강의 등록</a>
-    </div>
-    <div class="row">
-      <div class="col-6 col-md-4 col-lg-3" v-for="lectureList in list.data" :key="lectureList.lecNum">
-        <div class="card mb-3">
-          <a :href="`/lectureDetail/${lectureList.lecNum}`">
-            <div class="img-wrapper">
-              <img :src="`/project/api/lecture/${lectureList.imageNum}/image?imageNum=${lectureList.imageNum}`">
+    <div class="container">
+      <div class="row">
+        <lectureSide/>
+        <div class="col-md-8 col-lg-9">
+          <div class="row">
+            <div class="col-12">
+              <div class="d-flex justify-content-end mb-3">
+                <a href="/lectureUploadForm">강의 등록</a>
+              </div>
             </div>
-          </a>
-          <div class="card-body">
-            <h5>{{ lectureList.imageNum }}</h5>
-            <h5>{{ lectureList.title }}</h5>
-            <p><strong>{{ lectureList.teacher }}</strong></p>
-          </div>
+            <div class="col-6 col-md-4 col-lg-3" v-for="lectureList in list.data" :key="lectureList.lecNum">
+              <div class="card mb-3">
+                <a :href="`/lectureDetail/${lectureList.lecNum}`">
+                  <div class="img-wrapper">
+                    <img :src="`/project/api/lecture/${lectureList.imageNum}/image?imageNum=${lectureList.imageNum}`">
+                  </div>
+                </a>
+                <div class="card-body">
+                  <div>
+                    <h5>{{ lectureList.title }}</h5>
+                    <p><strong>{{ lectureList.teacher }}</strong></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <nav>
+            <ul class="pagination justify-content-center">
+              <li class="page-item" v-if="list.startPageNum != 1">
+                <a class="page-link new-page-link" @click="paging(list.startPageNum - 1)">
+                  <span aria-hidden="true">&lt;</span>
+                </a>
+              </li>
+
+              <li v-for="i in 5" :key="i" class="page-item ${list.pageNum eq i ? 'active' : '' }">
+                <a class="page-link new-page-link" v-if="i + list.startPageNum - 1 <= list.endPageNum"
+                  @click="paging(i + list.startPageNum - 1)">{{ i + list.startPageNum - 1 }}</a>
+              </li>
+
+              <li class="page-item" v-if="list.endPageNum < list.totalPage">
+                <a class="page-link new-page-link" @click="paging(list.endPageNum + 1)">
+                  <span aria-hidden="true">&gt;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
-    <nav>
-      <ul class="pagination justify-content-center">
-        <li class="page-item" v-if="list.startPageNum != 1">
-          <a class="page-link new-page-link" @click="paging(list.startPageNum - 1)">
-            <span aria-hidden="true">&lt;</span>
-          </a>
-        </li>
-
-        <li v-for="i in 5" :key="i" class="page-item ${list.pageNum eq i ? 'active' : '' }">
-          <a class="page-link new-page-link" v-if="i + list.startPageNum - 1 <= list.endPageNum"
-            @click="paging(i + list.startPageNum - 1)">{{ i + list.startPageNum - 1 }}</a>
-        </li>
-
-        <li class="page-item" v-if="list.endPageNum < list.totalPage">
-          <a class="page-link new-page-link" @click="paging(list.endPageNum + 1)">
-            <span aria-hidden="true">&gt;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+  </div>
   </div>
 </template>
+
 
     
   <script>
