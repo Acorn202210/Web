f64b6f2e12dab97b900a0fcc153ac94ac70fb7aa
OkHui <100889562+1000oki@users.noreply.github.com>
2023-03-17 12:50:10 +0900

    홈화면 구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,71 @@
 <template>
-    <div class="visual-bg">
-        <div class="container hero">
-            <div class="row">
-                <div class="col-12 col-xl-6 ">
-                    <h1 style="color:white">Programing Lecture Homepage</h1>
-                    <p class="mt-2" style="color:white">Acorn 2조</p>
-                    <!-- <button class="btn btn-light btn-lg action-button" type="button">Learn More</button> -->
-                </div>                
-            </div>
+  <div class="visual-bg">
+    <div class="container hero">
+      <div class="row">
+        <div class="col-12 col-xl-6 ">
+          <h1 style="color:white">Programing Lecture Homepage</h1>
+          <p class="mt-2" style="color:white">Acorn 2조</p>
+          <!-- <button class="btn btn-light btn-lg action-button" type="button">Learn More</button> -->
         </div>
       </div>
-      <div class="container">
-        <p class="mt-3">프론트 강의</p>
-        <div class="row">
-        <div class="col-3" v-for="tmp in front.data" :key="tmp.lecNum">
-            <div class="card mb-3" >
-            <a :href="`/lectureDetail/${tmp.lecNum}`">
-                <div class="img-wrapper" style="width:100%">
-                <img :src="`/project/api/lecture/${tmp.imageNum}/image?imageNum=${tmp.imageNum}`">
-                </div>
-            </a>
-            </div>
-        </div>
-        </div>
     </div>
-  </template>
+  </div>
+  <div class="container">
+    <div class="wrap">
+      <ul>
+        <p class="mt-3">프론트 강의</p>
+        <li v-for="tmp in front.data" :key="tmp.lecNum">
+          <div class="contain mb-3">
+            <a :href="`/lectureDetail/${tmp.lecNum}`">
+              <img class="home-img" :src="`/project/api/lecture/${tmp.imageNum}/image?imageNum=${tmp.imageNum}`">
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="wrap">
+      <ul>
+        <p>백엔드 강의</p>
+        <li v-for="tmp in back.data" :key="tmp.lecNum">
+          <div class="contain mb-3">
+            <a :href="`/lectureDetail/${tmp.lecNum}`">
+              <img class="home-img" :src="`/project/api/lecture/${tmp.imageNum}/image?imageNum=${tmp.imageNum}`">
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="wrap">
+      <ul>
+        <p>모바일 강의</p>
+        <li v-for="tmp in mobile.data" :key="tmp.lecNum">
+          <div class="contain mb-3">
+            <a :href="`/lectureDetail/${tmp.lecNum}`">
+              <img class="home-img" :src="`/project/api/lecture/${tmp.imageNum}/image?imageNum=${tmp.imageNum}`">
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
   
-  <script>
-  import axios from 'axios';
+<script>
+import axios from 'axios';
 
-  export default {
-      name: 'Home',
-      data() {
+export default {
+  name: 'Home',
+  data() {
     return {
       front: {
         data: [],
       },
+      back:{
+        data:[]
+      },
+      mobile:{
+        data:[]
+      }
     };
   },
   created() {
@@ -53,11 +83,40 @@
       })
       .catch(function (error) {
         console.log(error);
+      });
+    
+    const data2 = {
+      limit: 5,
+      largeCategory: 'backend',
+      smallCategory: 'all'
+    }
+    axios.get(url, { params: data2 })
+      .then(function (response) {
+        console.log(response.data);
+        vm.back = response.data.body;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+    const data3 = {
+      limit: 5,
+      largeCategory: 'mobile',
+      smallCategory: 'all'
+    }
+    axios.get(url, { params: data3 })
+      .then(function (response) {
+        console.log(response.data);
+        vm.mobile = response.data.body;
+      })
+      .catch(function (error) {
+        console.log(error);
       })
   },
-  }
-  </script>
   
-  <style>
-  @import '../assets/css/lecture.css'
-  </style>
+}
+</script>
+  
+<style>
+@import '../assets/css/lecture.css'
+</style>
