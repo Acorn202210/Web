b9ad1446a464162299f54a89cc16ad30690aa820
seo0601 <116258575+seo0601@users.noreply.github.com>
2023-03-23 17:39:29 +0900

    lectureSide 수정

--- src/components/common/lectureSide.vue
+++ src/components/common/lectureSide.vue
@@ -2,10 +2,10 @@
   <div class="col-md-2">
     <h5><a :class="{ 'bold-link': activeLink === 'front-all' }" @click="setActiveLink('front-all')">웹프론트엔드</a></h5>
     <a :class="{ 'bold-link': activeLink === 'front-js' }" @click="setActiveLink('front-js')">javascript</a>
-    <br><a :class="{ 'bold-link': activeLink === 'front-html-css' }" @click="setActiveLink('front-html_css')">html/css</a>
+    <br><a :class="{ 'bold-link': activeLink === 'front-html_css' }" @click="setActiveLink('front-html_css')">html/css</a>
     <br><a :class="{ 'bold-link': activeLink === 'front-react' }" @click="setActiveLink('front-react')">react</a>
     <br><a :class="{ 'bold-link': activeLink === 'front-vue' }" @click="setActiveLink('front-vue')">vue</a>
-    <br><a :class="{ 'bold-link': activeLink === 'front-jQuery' }" @click="setActiveLink('front-jquery')">jquery</a>
+    <br><a :class="{ 'bold-link': activeLink === 'front-jquery' }" @click="setActiveLink('front-jquery')">jquery</a>
     <br><br><h5><a :class="{ 'bold-link': activeLink === 'backend-all' }" @click="setActiveLink('backend-all')">웹백엔드</a></h5>
     <a :class="{ 'bold-link': activeLink === 'backend-java' }" @click="setActiveLink('backend-java')">java</a>
     <br><a :class="{ 'bold-link': activeLink === 'backend-spring' }" @click="setActiveLink('backend-spring')">spring</a>
